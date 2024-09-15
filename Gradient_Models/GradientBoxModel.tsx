eb10145c3014789d421da6a121afb82d6e473c85
Lakkireddy Narayana Reddy <narayanareddylakkireddy096@gmail.com>
2024-09-15 05:21:59 +0000

    different gradient Models

---
+++ Gradient_Models/GradientBoxModel.tsx
@@ -0,0 +1,12 @@
+import "./GradientBoxModel.css";
+
+export const GradientBoxModel = () => {
+  return (
+    <div className="container">
+      <div className="box-1" id="box"></div>
+      <div className="box-2" id="box"></div>
+      <div className="box-3" id="box"></div>
+      <div className="box-4" id="box"></div>
+    </div>
+  );
+};
