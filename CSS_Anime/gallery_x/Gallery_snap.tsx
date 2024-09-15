48d02b184a6d38c69684630174f5b590ffe2b3af
Lakkireddy Narayana Reddy <narayanareddylakkireddy096@gmail.com>
2024-09-15 03:09:31 +0000

    scroll gallery Css

---
+++ CSS_Anime/gallery_x/Gallery_snap.tsx
@@ -0,0 +1,20 @@
+import "./Gallery_snap.css";
+
+export const Gallery_snap = () => {
+  return (
+    <div className="container">
+      <div className="item item1">
+        <h1>Item1</h1>
+      </div>
+      <div className="item item2">
+        <h1>Item2</h1>
+      </div>
+      <div className="item item3">
+        <h1>Item3</h1>
+      </div>
+      <div className="item item4">
+        <h1>Item4</h1>
+      </div>
+    </div>
+  );
+};
---
+++ CSS_Anime/gallery_y/Gallery_Overlap.tsx
@@ -0,0 +1,12 @@
+import "./Gallery_Overlap.css";
+
+export const Gallery_Overlap = () => {
+  return (
+    <div className="container">
+      <div className="item item1">First Item</div>
+      <div className="item item2">Second Item</div>
+      <div className="item item3">Third Item</div>
+      <div className="item item4">Fourth Item</div>
+    </div>
+  );
+};
