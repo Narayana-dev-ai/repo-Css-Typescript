9080869e0f01bc1a39425525c1e905e4962349b1
Lakkireddy Narayana Reddy <narayanareddylakkireddy096@gmail.com>
2024-09-15 05:18:30 +0000

    Nav Bar Button Animation

---
+++ NavBar_Button_Anime/NavButtons.tsx
@@ -0,0 +1,11 @@
+import "./NavButtons.css";
+
+export const NavButtons = () => {
+  return (
+    <div className="wrapper">
+      <button className="icon">Instagram</button>
+      <button className="icon">Facebook</button>
+      <button className="icon">Twitter</button>
+    </div>
+  );
+};
